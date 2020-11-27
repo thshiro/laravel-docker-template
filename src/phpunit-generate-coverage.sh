636ad28e3fe58da95cee79aa136a42350019a530
Thalisson Barbosa <thalisson.s.barbosa@gmail.com>
2020-11-27 03:22:59 -0300

    Remove laravel files

--- src/phpunit-generate-coverage.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-sed -i "s|throttle:60,1|throttle:500,1|g" ./app/Http/Kernel.php
-phpdbg -qrr -d memory_limit=-1 vendor/bin/phpunit --configuration phpunit.xml --coverage-clover ./phpunit-coverage.xml --coverage-html ./public/coverage
-sed -i "s|throttle:500,1|throttle:60,1|g" ./app/Http/Kernel.php
